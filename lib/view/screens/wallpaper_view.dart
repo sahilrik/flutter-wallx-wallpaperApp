0e3d8921a1a0e9d20cd5e6c865378ab35cbb2c85
Sahil Barua <sahilrik@gmail.com>
2022-02-07 23:52:49 +0530

    implemented cache manager

--- lib/view/screens/wallpaper_view.dart
+++ lib/view/screens/wallpaper_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wallpaperapp/controller/wallpaper_controller.dart';
 
 import 'package:wallpaperapp/model/wallpaper.dart';
 import 'package:wallpaperapp/view/utils/helpers/colors_helper.dart';
-import 'package:wallpaperapp/view/utils/helpers/style_helper.dart';
+
+import 'package:wallpaperapp/view/wigets/wallpaper.dart';
 
 class WallpaperView extends StatelessWidget {
   final Wallpaper wallpaper;
@@ -42,151 +44,39 @@ class WallpaperView extends StatelessWidget {
                   onpressed: () {},
                   iconData: Icons.search_off_outlined,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: pinkcolor,
-                      child: WallpaperViewButton(
-                          color: whitecolor,
-                          onpressed: () {},
-                          iconData: Icons.file_download_outlined),
-                    ),
-                    const SetButton(),
-                    CircleAvatar(
-                      backgroundColor: whitecolor,
-                      child: WallpaperViewButton(
-                          color: pinkcolor,
-                          onpressed: () {},
-                          iconData: Icons.favorite_border),
-                    )
-                  ],
+                GetBuilder<WallpaperController>(
+                  init: WallpaperController(),
+                  builder: (controller) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: pinkcolor,
+                          child: WallpaperViewButton(
+                              color: whitecolor,
+                              onpressed: () {
+                                controller.downloadTheWallpaper(
+                                    wallpaper.urls.regular);
+                              },
+                              iconData: Icons.file_download_outlined),
+                        ),
+                        const SetButton(),
+                        CircleAvatar(
+                          backgroundColor: whitecolor,
+                          child: WallpaperViewButton(
+                              color: pinkcolor,
+                              onpressed: () {},
+                              iconData: Icons.favorite_border),
+                        )
+                      ],
+                    );
+                  },
                 )
               ],
             ),
           )
         ],
       ),
-    );
-  }
-}
-
-class SetButton extends StatelessWidget {
-  const SetButton({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        showBottomSheet(
-            backgroundColor: Colors.transparent,
-            context: context,
-            builder: (context) => Container(
-                  decoration: const BoxDecoration(
-                    color: whitecolor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                  ),
-                  height: Get.height * 0.4,
-                  child: ListView(
-                    children: [
-                      ListTile(
-                        onTap: () {
-                          Get.back();
-                        },
-                        leading: const Text(
-                          'set wallpaper as :',
-                          style: h1,
-                        ),
-                        trailing: const Icon(
-                          Icons.cancel_outlined,
-                          color: blackcolor,
-                        ),
-                      ),
-                      ListTile(
-                        onTap: () {
-                          Get.back();
-                        },
-                        leading: const Icon(
-                          Icons.home_outlined,
-                          color: blackcolor,
-                        ),
-                        title: const Text(
-                          'homescreen',
-                          style: h1,
-                        ),
-                      ),
-                      ListTile(
-                        onTap: () {
-                          Get.back();
-                        },
-                        leading: const Icon(
-                          Icons.lock_open,
-                          color: blackcolor,
-                        ),
-                        title: const Text(
-                          'LockScreen',
-                          style: h1,
-                        ),
-                      ),
-                      ListTile(
-                        onTap: () {
-                          Get.back();
-                        },
-                        leading: const Icon(
-                          Icons.screen_lock_landscape,
-                          color: blackcolor,
-                        ),
-                        title: const Text(
-                          'both ',
-                          style: h1,
-                        ),
-                      ),
-                    ],
-                  ),
-                ));
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          vertical: 15,
-          horizontal: 80.0,
-        ),
-        child: const Text(
-          'set as',
-          style: buttons,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          color: pinkcolor,
-        ),
-      ),
-    );
-  }
-}
-
-class WallpaperViewButton extends StatelessWidget {
-  final Color color;
-  final Function onpressed;
-  final IconData iconData;
-  const WallpaperViewButton(
-      {Key? key,
-      required this.color,
-      required this.onpressed,
-      required this.iconData})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        onpressed();
-      },
-      icon: Icon(iconData),
-      color: color,
     );
   }
 }
